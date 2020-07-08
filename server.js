fba112f81d9f7fe5df9ab65c70384f2743025946
luukkonen1991 <luukkonen1991@gmail.com>
2020-07-08 21:51:07 +0300

    ADDED sequelize & connection to database

--- server.js
+++ server.js
@@ -30,3 +30,10 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`App listening on port ${process.env.PORT} on mode ${process.env.NODE_ENV}`);
 });
+
+//HANDLE UNHANDELED PROMISE REJECTIONS
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  //CLOSE SERVER & EXIT PROCESS
+  server.close(() => process.exit(1));
+});
---
+++ utils/database.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+const Sequelize = require('sequelize');
+
+// LOAD ENV VARS
+dotenv.config({ path: '../config/config.env' });
+
+const sequelize = new Sequelize(process.env.PSQL_DATABASE, process.env.PSQL_USERNAME, process.env.PSQL_PASSWORD, {
+  host: process.env.PSQL_HOST,
+  dialect: 'postgres'
+});
+
+module.exports = sequelize;
