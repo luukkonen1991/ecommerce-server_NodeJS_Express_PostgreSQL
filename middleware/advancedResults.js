5e60f0e0a640a2feef5de68eed9effb3cac49dd8
luukkonen1991 <luukkonen1991@gmail.com>
2020-07-10 17:53:45 +0300

    FINALLY working versio of advancedResults middleware to sort by category and gt/lt price

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,40 +1,34 @@
 const { Op } = require('sequelize');
 
 const advancedResults = (model) => async (req, res, next) => {
-  let query;
-
-  // Copy req.query
-  const reqQuery = { ...req.query };
-  // console.log(reqQuery);
-
-  // Fields to exlude
-  const removeFields = ['price', 'gt'];
-  // Loop over removeFields and delete em from reqQuery
-  removeFields.forEach(param => delete reqQuery[param]);
-  // console.log(reqQuery);
-
-  // Make query to string in order to manipulate it
-  let queryStr = JSON.stringify(reqQuery);
-  console.log(queryStr);
-
-  if (req.query.price && req.query.gt) {
-    let obj = { [Op.gt]: req.query.gt };
-    let queryObj = { where: JSON.parse(queryStr), price: obj };
-    console.log(queryObj);
-    query = model.findAll(queryObj);
-  };
-
-
-  let tämä2 = {
+  let query = {
     where: {
-      category: 't-shirts',
-      price: { [Op.gt]: 12 }
     }
   };
 
-  console.log(tämä2, 'töööö');
+  // // Copy req.query
+  // const reqQuery = { ...req.query };
 
-  const results = await query;
+  // // Fields to exlude
+  // const removeFields = [];
+  // // Loop over removeFields and delete em from reqQuery
+  // removeFields.forEach(param => delete reqQuery[param]);
+
+  if (req.query.category) {
+    query.where.category = req.query.category;
+  }
+
+  if (req.query.price && req.query.gt) {
+    query.where.price = { [Op.gt]: req.query.price };
+  }
+
+  if (req.query.price && req.query.lt) {
+    query.where.price = { [Op.lt]: req.query.price };
+  }
+
+  finalQuery = model.findAll(query);
+
+  const results = await finalQuery;
 
   res.advancedResults = {
     success: true,
